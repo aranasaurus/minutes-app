c21905c6406ac9a92553ae180ead6a6bd06bfe5c
Ryan Arana <ryan.arana@gmail.com>
2017-03-18 21:35:40 -0700

    More fun with colors

--- minutes/code/common/Colors.swift
+++ minutes/code/common/Colors.swift
@@ -94,11 +94,11 @@ struct Colors {
 //    static let actionable = Color(#colorLiteral(red: 0.9450980392, green: 0.9490196078, blue: 0.9215686275, alpha: 1))
 //}
 
-// Neutral Testbed
+// Turquoise
 extension Colors {
-    private static let hue = CGFloat(149.0/360.0)
-    private static let sat = CGFloat(1)
-    private static let bri = CGFloat(0.6)
+    private static let hue = CGFloat(190.0/360.0)
+    private static let sat = CGFloat(0.8)
+    private static let bri = CGFloat(0.77)
 
     static let primary = Color(
         base: UIColor(hue: hue, saturation: sat, brightness: bri, alpha: 1),
@@ -109,20 +109,61 @@ extension Colors {
 
     static let contrast = Color(
         UIColor(
-            hue: hue <= 0.5 ? hue + 0.5 : (hue + 0.5).remainder(dividingBy: 1),
-            saturation: sat,
-            brightness: 1,
+            hue: hue.shifted(by: 0.3667),
+            saturation: sat * 0.9,
+            brightness: bri * 1.2,
             alpha: 1
         )
     )
 
     static let background = Color(
         base: UIColor(hue: hue, saturation: 0.00, brightness: 0.7, alpha: 1),
-        light: UIColor(hue: hue, saturation: sat * 0.005, brightness: 0.97, alpha: 1),
-        dark: UIColor(hue: hue, saturation: sat * 0.25, brightness: 0.5, alpha: 1),
+        light: UIColor(hue: hue.shifted(by: 0.6667), saturation: sat * 0.04, brightness: 0.98, alpha: 1),
+        dark: UIColor(hue: hue.shifted(by: 0.6667), saturation: sat * 0.125, brightness: 0.333, alpha: 1),
         highlighted: UIColor(hue: hue, saturation: 0.005, brightness: 1, alpha: 1)
     )
 
     static let negative = Color(.red)
     static let positive = Color(.green)
+}
+
+// Testbed
+//extension Colors {
+//    private static let hue = CGFloat(190.0/360.0)
+//    private static let sat = CGFloat(0.8)
+//    private static let bri = CGFloat(0.77)
+//
+//    static let primary = Color(
+//        base: UIColor(hue: hue, saturation: sat, brightness: bri, alpha: 1),
+//        light: UIColor(hue: hue, saturation: sat/3, brightness: 1, alpha: 1),
+//        dark: UIColor(hue: hue, saturation: min(1, sat*1.2), brightness: 0.33, alpha: 1),
+//        actionable: UIColor(hue: hue, saturation: 0.04, brightness: 1, alpha: 1)
+//    )
+//
+//    static let contrast = Color(
+//        UIColor(
+//            hue: hue.shifted(by: 0.3667),
+//            saturation: sat * 0.9,
+//            brightness: bri * 1.2,
+//            alpha: 1
+//        )
+//    )
+//
+//    static let background = Color(
+//        base: UIColor(hue: hue, saturation: 0.00, brightness: 0.7, alpha: 1),
+//        light: UIColor(hue: hue.shifted(by: 0.6667), saturation: sat * 0.04, brightness: 0.98, alpha: 1),
+//        dark: UIColor(hue: hue.shifted(by: 0.6667), saturation: sat * 0.125, brightness: 0.333, alpha: 1),
+//        highlighted: UIColor(hue: hue, saturation: 0.005, brightness: 1, alpha: 1)
+//    )
+//
+//    static let negative = Color(.red)
+//    static let positive = Color(.green)
+//}
+
+extension CGFloat {
+    func shifted(by amount: CGFloat = 0.5, around: CGFloat = 1) -> CGFloat {
+        let result = self <= around - amount ? self + amount : abs((self + amount).remainder(dividingBy: around))
+//        print("shifting \(self)[\(self * 360)] by \(amount): \(result)[\(result * 360)]")
+        return result
+    }
 }
